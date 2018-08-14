5e7b6b7f532b22d07f04f3c55e82ab2f8b023448
Delson Tan <delsonwtan@gmail.com>
2018-08-14 19:51:39 +0000

    Complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,8 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+    
+puts @store1 = Store.first
+puts @store2 = Store.second
 
-# Your code goes here ...
+@store1.update(annual_revenue: 360000)
