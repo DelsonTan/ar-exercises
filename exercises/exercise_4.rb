23efdc1e1ade4fdbbcf2c6c212315c8ba4dd2cdc
Delson Tan <delsonwtan@gmail.com>
2018-08-14 20:05:13 +0000

    Complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,14 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true
+Store.create name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false
+Store.create name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where(womens_apparel: true)
+
+@mens_stores.map { |store| puts "[Men's Stores] Annual revenue for #{store.name}: $#{store.annual_revenue}" }
+@womens_stores.map { |store| puts "[Women's Stores] Annual revenue for #{store.name}: $#{store.annual_revenue} (less than $1M)" if store.annual_revenue < 1000000}
